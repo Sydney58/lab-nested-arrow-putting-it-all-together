579b11e9ff2a1e2fb909c1796499d8ceb53380b7
sydney58 <sydneymunene58@gmail.com>
2025-11-28 13:44:53 +0300

    first test

--- index.js
+++ index.js
@@ -1,4 +1,35 @@
+function createLoginTracker(userInfo) {
+  let attemptCount = 0;
 
+  const attemptLogin = (passwordAttempt) => {
+    attemptCount++;
+    if (attemptCount > 3) {
+      return "Account locked due to too many failed login attempts";
+    }
+    if (passwordAttempt === userInfo.password) {
+      return "Login successful";
+    }
+    return `Attempt ${attemptCount}: Login failed`;
+  };
+  return attemptLogin;
+}
+
+//example use
+const login = createLoginTracker({
+  username: "user1",
+  password: "pass123"
+});
+
+console.log(login("wrong"));  
+console.log(login("wrong")); 
+console.log(login("pass123"));
+console.log(login("wrong")); 
+console.log(login("pass123"));
+
+
+
+module.exports = {...(typeof createLoginTracker !== 'undefined' && { createLoginTracker })
+};
 
 
 module.exports = {
